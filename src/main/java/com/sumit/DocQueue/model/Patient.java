ddd64aa57dd2a84d2db24a1fa3ae4f058b43d4f4
Sumit031202 <sumitkr.031202@gmail.com>
2026-04-14 19:06:11 +0530

    feat: add Patient entity for storing visitor details

---
+++ src/main/java/com/sumit/DocQueue/model/Patient.java
@@ -0,0 +1,22 @@
+package com.sumit.DocQueue.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "patients")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String contact;
+    private Integer age;
+
+}
