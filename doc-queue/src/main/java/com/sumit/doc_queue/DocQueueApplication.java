febea75200583c70db98c2fffecc33e5a5f4718d
Sumit031202 <sumitkr.031202@gmail.com>
2026-06-05 00:03:06 +0530

    Initial commit: Setup DocQueue project framework

---
+++ doc-queue/src/main/java/com/sumit/doc_queue/DocQueueApplication.java
@@ -0,0 +1,13 @@
+package com.sumit.doc_queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocQueueApplication.class, args);
+	}
+
+}
---
+++ doc-queue/src/test/java/com/sumit/doc_queue/DocQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumit.doc_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
