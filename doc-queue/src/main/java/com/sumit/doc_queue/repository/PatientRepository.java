605a14cbe6b490aa6689f62be18123716b182d78
Sumit031202 <sumitkr.031202@gmail.com>
2026-06-05 14:33:07 +0530

    feat: implement doctor call-next-patient logic

--- doc-queue/src/main/java/com/sumit/doc_queue/repository/PatientRepository.java
+++ doc-queue/src/main/java/com/sumit/doc_queue/repository/PatientRepository.java
@@ -1,8 +1,11 @@
 package com.sumit.doc_queue.repository;
 
 import com.sumit.doc_queue.model.Patient;
+import com.sumit.doc_queue.model.QueueStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PatientRepository extends JpaRepository<Patient,Long> {
+import java.util.List;
 
+public interface PatientRepository extends JpaRepository<Patient,Long> {
+    List<Patient> findByStatusOrderByArrivalTime(QueueStatus status);
 }
--- doc-queue/src/main/java/com/sumit/doc_queue/service/QueueService.java
+++ doc-queue/src/main/java/com/sumit/doc_queue/service/QueueService.java
@@ -1,8 +1,11 @@
 package com.sumit.doc_queue.service;
 
 import com.sumit.doc_queue.model.Patient;
+import com.sumit.doc_queue.model.QueueStatus;
 import com.sumit.doc_queue.repository.PatientRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class QueueService {
@@ -18,5 +21,18 @@ public class QueueService {
         patientRepository.save(p);
         System.out.println(p.getFullName()+" is saved in the database");
         return p;
+    }
+
+    public Optional<Patient> callNextPatient(){
+        List<Patient> patientList=patientRepository.findByStatusOrderByArrivalTime(QueueStatus.WAITING);
+        if(patientList.isEmpty()){
+            return Optional.empty();
+        }else{
+            Patient p=patientList.get(0);
+            // call the patient
+            p.setStatus(QueueStatus.IN_PROGRESS);
+            patientRepository.save(p);
+            return Optional.of(p);
+        }
     }
 }
