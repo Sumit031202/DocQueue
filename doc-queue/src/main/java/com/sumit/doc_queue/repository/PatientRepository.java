cc40acf525b4979af65dfae5a34c60a22471075c
Sumit031202 <sumitkr.031202@gmail.com>
2026-08-05 22:47:35 +0530

    feat:patient repository can fetch data acc to doc id

--- doc-queue/src/main/java/com/sumit/doc_queue/repository/PatientRepository.java
+++ doc-queue/src/main/java/com/sumit/doc_queue/repository/PatientRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PatientRepository extends JpaRepository<Patient,Long> {
-    List<Patient> findByStatusOrderByArrivalTime(QueueStatus status);
+    List<Patient> findByDoctorIdAndStatusOrderByArrivalTime(Long doctorId,QueueStatus status);
 }
