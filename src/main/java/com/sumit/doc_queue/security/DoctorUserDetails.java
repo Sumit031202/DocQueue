54065cce22992a814aaabc3b58480857aeb2b103
Sumit031202 <sumitkr.031202@gmail.com>
2026-09-08 00:21:59 +0530

    feat: add DoctorUserDetails security adapter

---
+++ src/main/java/com/sumit/doc_queue/security/DoctorUserDetails.java
@@ -0,0 +1,28 @@
+package com.sumit.doc_queue.security;
+
+import com.sumit.doc_queue.model.Doctor;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+public class DoctorUserDetails implements UserDetails {
+    private final Doctor doctor;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return doctor.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return doctor.getEmail();
+    }
+}
