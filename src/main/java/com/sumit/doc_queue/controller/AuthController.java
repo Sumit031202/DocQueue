322db6bff7226485e5c848848e1b067da1e2a744
Sumit031202 <sumitkr.031202@gmail.com>
2026-09-07 20:20:48 +0530

    feat: add doctor registration endpoint

---
+++ src/main/java/com/sumit/doc_queue/controller/AuthController.java
@@ -0,0 +1,19 @@
+package com.sumit.doc_queue.controller;
+
+import com.sumit.doc_queue.dto.DoctorRegistrationRequest;
+import com.sumit.doc_queue.dto.DoctorResponse;
+import com.sumit.doc_queue.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+    @PostMapping("/register")
+    public DoctorResponse registerDoctor(@Valid @RequestBody DoctorRegistrationRequest doctorRegistrationRequest){
+        return authService.register(doctorRegistrationRequest);
+    }
+}
