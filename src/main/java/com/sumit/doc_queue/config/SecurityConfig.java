f795868b15c400b0f60948cf4ddb1d95f5b9db51
Sumit031202 <sumitkr.031202@gmail.com>
2026-09-08 00:59:55 +0530

    feat: add authentication manager

--- src/main/java/com/sumit/doc_queue/config/SecurityConfig.java
+++ src/main/java/com/sumit/doc_queue/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.sumit.doc_queue.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,5 +23,10 @@ public class SecurityConfig {
                         auth.requestMatchers("/api/auth/register").permitAll()
                                 .anyRequest().authenticated());
         return http.build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
     }
 }
