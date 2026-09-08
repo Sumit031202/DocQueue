40b8a033613e35d16a9f84794c419aebdea6c848
Sumit031202 <sumitkr.031202@gmail.com>
2026-09-09 00:30:04 +0530

    feat: add doctor login with jwt authentication

--- src/main/java/com/sumit/doc_queue/service/AuthService.java
+++ src/main/java/com/sumit/doc_queue/service/AuthService.java
@@ -1,10 +1,15 @@
 package com.sumit.doc_queue.service;
 
+import com.sumit.doc_queue.dto.DoctorLoginRequest;
 import com.sumit.doc_queue.dto.DoctorRegistrationRequest;
 import com.sumit.doc_queue.dto.DoctorResponse;
 import com.sumit.doc_queue.model.Doctor;
 import com.sumit.doc_queue.repository.DoctorRepository;
+import com.sumit.doc_queue.security.JwtService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +18,8 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final DoctorRepository doctorRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager manager;
+    private final JwtService jwtService;
 
     public DoctorResponse register(DoctorRegistrationRequest doctor){
         if(doctorRepository.existsByEmail(doctor.getEmail())){
@@ -26,5 +33,11 @@ public class AuthService {
             Doctor savedDoctor=doctorRepository.save(newDoctor);
             return new DoctorResponse(savedDoctor.getId(),savedDoctor.getName(),savedDoctor.getSpecialization());
         }
+    }
+    public String login(DoctorLoginRequest doctorLoginRequest){
+        Authentication authentication=new UsernamePasswordAuthenticationToken(doctorLoginRequest.getEmail(),doctorLoginRequest.getPassword());
+        Authentication authenticated=manager.authenticate(authentication);
+        String email=authenticated.getName();
+        return jwtService.generateToken(email);
     }
 }
