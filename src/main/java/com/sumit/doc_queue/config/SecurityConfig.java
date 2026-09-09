ba7d107aa7d35dba824c4117381e419765835957
Sumit031202 <sumitkr.031202@gmail.com>
2026-09-10 01:48:12 +0530

    feat: configure JWT security filter

--- src/main/java/com/sumit/doc_queue/config/SecurityConfig.java
+++ src/main/java/com/sumit/doc_queue/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.sumit.doc_queue.config;
 
+import com.sumit.doc_queue.security.JwtAuthenticationFilter;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,19 +10,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@AllArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.addFilterBefore(
+                jwtAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class
+        );
         http
                 .csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(auth->
-                        auth.requestMatchers("/api/auth/register").permitAll()
+                        auth.requestMatchers("/api/auth/**").permitAll()
                                 .anyRequest().authenticated());
         return http.build();
     }
