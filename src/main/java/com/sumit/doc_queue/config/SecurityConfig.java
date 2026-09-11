6c8878f57d36cf7c2280af6e59720553d57f53f9
Sumit031202 <sumitkr.031202@gmail.com>
2026-09-11 14:51:21 +0530

    feat: add role based authorization for Doctor Endpoints

--- src/main/java/com/sumit/doc_queue/config/SecurityConfig.java
+++ src/main/java/com/sumit/doc_queue/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                 .csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(auth->
                         auth.requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/api/doctors/**").hasRole("DOCTOR")
                                 .anyRequest().authenticated());
         return http.build();
     }
